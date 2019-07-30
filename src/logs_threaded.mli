ae5f5cd1d5ea12c52ae3565f8f2d81fc433ce23b
Jules Aguillon <juloo.dsi@gmail.com>
2019-07-30 15:32:24 +0200

    More general and automagical thread safety

---
+++ src/logs_threaded.mli
@@ -0,0 +1,6 @@
+(** Thread safe logging.
+
+    Linking this library will make Logs thread safe by using a global lock.
+    Setting [Logs.set_mutex] will override this.
+
+    {e %%VERSION%% - {{:%%PKG_HOMEPAGE%%}homepage}} *)
