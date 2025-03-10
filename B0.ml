a9b8d4ab1a38d5ac0191cf3901ac3afe57bc3d11
Daniel Bünzli <daniel.buenzli@erratique.ch>
2025-03-10 18:17:48 +0100

    Spring clean build infrastructure.

--- B0.ml
+++ B0.ml
@@ -2,6 +2,7 @@ open B0_kit.V000
 
 (* OCaml library names *)
 
+let compiler_libs_toplevel = B0_ocaml.libname "compiler-libs.toplevel"
 let mtime = B0_ocaml.libname "mtime"
 let mtime_clock = B0_ocaml.libname "mtime.clock"
 let unix = B0_ocaml.libname "unix"
@@ -21,97 +22,94 @@ let logs_browser = B0_ocaml.libname "logs.browser"
 let logs_cli = B0_ocaml.libname "logs.cli"
 let logs_lwt = B0_ocaml.libname "logs.lwt"
 let logs_threaded = B0_ocaml.libname "logs.threaded"
+let logs_top = B0_ocaml.libname "logs.top"
 
 (* Libraries *)
 
-let mod_srcs m =
-  let mli = Fmt.str "src/%s.mli" m and ml = Fmt.str "src/%s.ml" m in
-  Fpath.[ `File (v mli); `File (v ml) ]
-
 let logs_lib =
-  let srcs = mod_srcs "logs" in
-  B0_ocaml.lib logs ~doc:"The logs library" ~srcs ~requires:[]
+  B0_ocaml.lib logs ~srcs:[`Dir ~/"src"] ~requires:[]
 
 let logs_fmt_lib =
-  let srcs = mod_srcs "logs_fmt" in
-  let requires = [logs; fmt] in
-  B0_ocaml.lib logs_fmt ~doc:"The logs.fmt library" ~srcs ~requires
+  B0_ocaml.lib logs_fmt ~srcs:[`Dir ~/"src/fmt"] ~requires:[logs; fmt]
 
 let logs_browser_lib =
-  let srcs = mod_srcs "logs_browser" in
-  let requires = [logs; js_of_ocaml_compiler_runtime] in
-  B0_ocaml.lib logs_browser ~doc:"The logs.browser library" ~srcs ~requires
+  let srcs = [`Dir ~/"src/browser"] in
+  B0_ocaml.lib logs_browser ~srcs ~requires:[logs; js_of_ocaml_compiler_runtime]
 
 let logs_threaded_lib =
-  let srcs = mod_srcs "logs_threaded" in
-  let requires = [logs; threads] in
-  B0_ocaml.lib logs_threaded ~doc:"The logs.threaded library" ~srcs ~requires
+  let srcs = [`Dir ~/"src/threaded"] in
+  B0_ocaml.lib logs_threaded ~srcs ~requires:[logs; threads]
 
 let logs_cli_lib =
-  let srcs = mod_srcs "logs_cli" in
-  let requires = [logs; cmdliner] in
-  B0_ocaml.lib logs_cli ~doc:"The logs.cli library" ~srcs ~requires
+  B0_ocaml.lib logs_cli ~srcs:[`Dir ~/"src/cli"] ~requires:[logs; cmdliner]
 
 let logs_lwt_lib =
-  let srcs = mod_srcs "logs_lwt" in
-  let requires = [logs; lwt] in
-  B0_ocaml.lib logs_lwt ~doc:"The logs.lwt library" ~srcs ~requires
+  B0_ocaml.lib logs_lwt ~srcs:[`Dir ~/"src/lwt"] ~requires:[logs; lwt]
+
+let logs_top_lib =
+  let srcs = [`Dir ~/"src/top"] in
+  B0_ocaml.lib logs_top ~srcs ~requires:[logs; compiler_libs_toplevel]
 
 (* Tests *)
 
-let test ?doc base ~requires =
-  let srcs = Fpath.[`File (v (Fmt.str "test/%s.ml" base))] in
-  B0_ocaml.exe base ?doc ~srcs ~requires
+let test ?(requires = []) = B0_ocaml.test ~requires:(logs :: requires)
 
-let test_fmt = test "test_fmt" ~requires:[logs; logs_fmt; fmt_tty]
+let test_fmt =
+  let requires = [fmt_tty; logs_fmt] in
+  test ~/"test/test_fmt.ml" ~requires ~run:false (* exits with 1. *)
+
+let test_formatter =
+  test ~/"test/test_formatter.ml" ~run:false (* exits with 1. *)
+
 let test_tool =
-  let requires = [logs; logs_fmt; logs_cli; fmt_cli; fmt_tty; cmdliner] in
-  test "tool" ~requires
+  let requires = [logs_fmt; logs_cli; fmt_cli; fmt_tty; cmdliner] in
+  test ~/"test/tool.ml" ~requires ~run:false (* exits with 1 *)
 
-let test_tags = test "tags" ~requires:[logs; mtime; mtime_clock]
-let test_formatter = test "test_formatter" ~requires:[logs]
-let test_multi = test "test_multi" ~requires:[logs; logs_fmt; fmt_tty]
+let test_tags =
+  test ~/"test/tags.ml" ~requires:[mtime; mtime_clock]
+
+let test_multi =
+  test ~/"test/test_multi.ml" ~requires:[logs; logs_fmt; fmt_tty]
+
 let test_threaded =
-  let requires = [logs; logs_fmt; logs_threaded; threads] in
-  test "test_threaded" ~requires
+  test ~/"test/test_threaded.ml" ~requires:[logs_fmt; logs_threaded; threads]
 
 let test_lwt =
-  let requires = [logs; logs_fmt; logs_lwt; fmt; fmt_tty; lwt; lwt_unix] in
-  test "test_lwt" ~requires
+  let requires = [logs_fmt; logs_lwt; fmt; fmt_tty; lwt; lwt_unix] in
+  test ~/"test/test_lwt.ml" ~requires ~run:false (* exits with 1 *)
 
 (* Packs *)
 
 let default =
   let meta =
     B0_meta.empty
-    |> B0_meta.(add authors) ["The logs programmers"]
-    |> B0_meta.(add maintainers)
-       ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> B0_meta.(add homepage) "https://erratique.ch/software/logs"
-    |> B0_meta.(add online_doc) "https://erratique.ch/software/logs/doc"
-    |> B0_meta.(add licenses) ["ISC"]
-    |> B0_meta.(add repo) "git+https://erratique.ch/repos/logs.git"
-    |> B0_meta.(add issues) "https://github.com/dbuenzli/logs/issues"
-    |> B0_meta.(add description_tags) ["log"; "system"; "org:erratique"; ]
+    |> ~~ B0_meta.authors ["The logs programmers"]
+    |> ~~ B0_meta.maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> ~~ B0_meta.homepage "https://erratique.ch/software/logs"
+    |> ~~ B0_meta.online_doc "https://erratique.ch/software/logs/doc"
+    |> ~~ B0_meta.licenses ["ISC"]
+    |> ~~ B0_meta.repo "git+https://erratique.ch/repos/logs.git"
+    |> ~~ B0_meta.issues "https://github.com/dbuenzli/logs/issues"
+    |> ~~ B0_meta.description_tags ["log"; "system"; "org:erratique"; ]
     |> B0_meta.tag B0_opam.tag
-    |> B0_meta.add B0_opam.build
+    |> ~~ B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"
           "--with-js_of_ocaml" "%{js_of_ocaml:installed}%"
           "--with-fmt" "%{fmt:installed}%"
           "--with-cmdliner" "%{cmdliner:installed}%"
           "--with-lwt" "%{lwt:installed}%"
           "--with-base-threads" "%{base-threads:installed}%"]]|}
-    |> B0_meta.add B0_opam.depopts
+    |> ~~ B0_opam.depopts
       ["cmdliner", "";
        "js_of_ocaml", "";
        "fmt", "";
        "lwt", "";
        "base-threads", ""]
     |> B0_meta.add B0_opam.conflicts [
-      "cmdliner", {|< "1.1.0"|};
+      "cmdliner", {|< "1.3.0"|};
       "js_of_ocaml", {|< "4.0.0"|};
       "fmt", {|< "0.9.0"|}; ]
-    |> B0_meta.add B0_opam.depends
+    |> ~~ B0_opam.depends
       [ "ocaml", {|>= "4.08.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
