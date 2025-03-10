dcd4868261444c8a3cd4d6a6249f7e60ac93bac7
Daniel Bünzli <daniel.buenzli@erratique.ch>
2025-03-10 17:39:18 +0100

    Also put the library sources in their own directory.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -8,7 +8,6 @@ let cmdliner = Conf.with_pkg "cmdliner"
 let fmt = Conf.with_pkg "fmt"
 let lwt = Conf.with_pkg "lwt"
 let threads = Conf.with_pkg "base-threads"
-
 let () =
   Pkg.describe "logs" @@ fun c ->
   let jsoo = Conf.value c jsoo in
@@ -17,24 +16,13 @@ let () =
   let lwt = Conf.value c lwt in
   let threads = Conf.value c threads in
   Ok [ Pkg.mllib "src/logs.mllib";
-       Pkg.mllib ~cond:fmt "src/logs_fmt.mllib" ~dst_dir:"fmt";
-       Pkg.mllib ~cond:jsoo "src/logs_browser.mllib" ~dst_dir:"browser";
-       Pkg.mllib ~cond:cmdliner "src/logs_cli.mllib" ~dst_dir:"cli";
-       Pkg.mllib ~cond:lwt "src/logs_lwt.mllib" ~dst_dir:"lwt";
-       Pkg.mllib ~cond:fmt ~api:[] "src/logs_top.mllib" ~dst_dir:"top";
-       Pkg.mllib ~cond:threads "src/logs_threaded.mllib" ~dst_dir:"threaded";
-       Pkg.lib "src/logs_top_init.ml";
-       Pkg.lib "src/logs_top_init.ml" ~dst:"top/logs_top_init_ml";
-       Pkg.lib "src/logs_fmt_top_init.ml" ~dst:"fmt/logs_fmt_top_init.ml";
-       Pkg.doc "test/tool.ml";
-       Pkg.doc "test/tags.ml";
-       Pkg.test "test/test_fmt";
-       Pkg.test "test/test_formatter";
-       Pkg.test "test/tool";
-       Pkg.test "test/tags";
-       Pkg.test "test/test_multi";
-(*       Pkg.test "test/test_browser.js";
-         Pkg.test "test/test_browser.html"; *)
-       Pkg.test "test/test_threaded";
-       Pkg.test "test/test_lwt";
- ]
+       Pkg.mllib ~cond:fmt "src/fmt/logs_fmt.mllib" ~dst_dir:"fmt";
+       Pkg.mllib ~cond:jsoo "src/browser/logs_browser.mllib" ~dst_dir:"browser";
+       Pkg.mllib ~cond:cmdliner "src/cli/logs_cli.mllib" ~dst_dir:"cli";
+       Pkg.mllib ~cond:lwt "src/lwt/logs_lwt.mllib" ~dst_dir:"lwt";
+       Pkg.mllib ~cond:fmt ~api:[] "src/top/logs_top.mllib" ~dst_dir:"top";
+       Pkg.mllib ~cond:threads "src/threaded/logs_threaded.mllib" ~dst_dir:"threaded";
+       Pkg.lib "src/top/logs_top_init.ml";
+       Pkg.lib "src/top/logs_top_init.ml" ~dst:"top/logs_top_init_ml";
+       Pkg.lib "src/fmt/logs_fmt_top_init.ml" ~dst:"fmt/logs_fmt_top_init.ml";
+       Pkg.doc "doc/index.mld" ~dst:"odoc-pages/index.mld"]
